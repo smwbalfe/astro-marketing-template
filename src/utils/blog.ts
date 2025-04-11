707b9a009217fef1c1b4e7b164ab82401aadc098
smwbalfe <smwbalfe@gmail.com>
2025-04-11 20:15:28 +0100

    remove comment

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -113,19 +113,16 @@ const load = async function (): Promise<Array<Post>> {
 
 let _posts: Array<Post>;
 
-/** */
 export const isBlogEnabled = APP_BLOG.isEnabled;
 export const isRelatedPostsEnabled = APP_BLOG.isRelatedPostsEnabled;
 export const isBlogListRouteEnabled = APP_BLOG.list.isEnabled;
 export const isBlogPostRouteEnabled = APP_BLOG.post.isEnabled;
 export const isBlogCategoryRouteEnabled = APP_BLOG.category.isEnabled;
 export const isBlogTagRouteEnabled = APP_BLOG.tag.isEnabled;
-
 export const blogListRobots = APP_BLOG.list.robots;
 export const blogPostRobots = APP_BLOG.post.robots;
 export const blogCategoryRobots = APP_BLOG.category.robots;
 export const blogTagRobots = APP_BLOG.tag.robots;
-
 export const blogPostsPerPage = APP_BLOG?.postsPerPage;
 
 /** */
