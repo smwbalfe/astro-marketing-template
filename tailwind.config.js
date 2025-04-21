b9e2cfbb9e4cb14ec04d80a8f604f95cb9d4bf80
smwbalfe <smwbalfe@gmail.com>
2025-04-21 16:18:16 +0100

    insert astroship UI + tweak font / styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,9 @@ export default {
         muted: 'var(--aw-color-text-muted)',
       },
       fontFamily: {
-        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        sans: ['Bricolage Grotesque', ...defaultTheme.fontFamily.sans],
+        serif: ['Bricolage Grotesque', ...defaultTheme.fontFamily.serif],
+        heading: ['Bricolage Grotesque', ...defaultTheme.fontFamily.sans],
       },
 
       animation: {
